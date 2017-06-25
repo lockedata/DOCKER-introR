52ae570734bd5507486947a4500b823f666fa066
Stephanie Locke <stephanie.g.locke@gmail.com>
2017-06-26 00:58:40 +0100

    No need to fetch

--- mkusers.sh
+++ mkusers.sh
@@ -3,5 +3,5 @@ do
         user=`echo $userdetails | cut -f 1 -d ,`
 	passwd=`echo $userdetails | cut -f 2 -d ,`
         useradd $user -m -p `mkpasswd $passwd` -G staff
-        curl https://raw.githubusercontent.com/lockedata/DOCKER-introR/master/sampleSQL.r /home/$user
+        cp sampleSQL.r /home/$user
 done
