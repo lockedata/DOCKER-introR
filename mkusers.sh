05e771c2fa4592736b8ce04a353788b44fbe7d53
Stephanie Locke <stephanie.g.locke@gmail.com>
2017-06-26 08:49:41 +0100

    Spaces just in case

--- mkusers.sh
+++ mkusers.sh
@@ -2,7 +2,7 @@ for userdetails in `cat users.csv`
 do
         user=`echo $userdetails | cut -f 1 -d ,`
 	passwd=`echo $userdetails | cut -f 2 -d ,`
-        useradd $user -m  -G staff -p `mkpasswd $passwd`
+        useradd $user -m -G staff -p `mkpasswd $passwd`
         cp sampleSQL.r /home/$user/
 done
 cut -d: -f1 /etc/passwd
