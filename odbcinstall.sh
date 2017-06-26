fd9a464bce351808dfa83265b432ed0fa8cf8b4a
Stephanie Locke <stephanie.g.locke@gmail.com>
2017-06-26 06:04:20 +0100

    Yes to install

--- odbcinstall.sh
+++ odbcinstall.sh
@@ -1,6 +1,6 @@
 apt-key adv --fetch-keys https://packages.microsoft.com/keys/microsoft.asc
 echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/16.04/prod xenial main" > /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
