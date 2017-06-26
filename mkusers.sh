a3c8088eb752ee714a9c976225c1efff61ff5ec8
Stephanie Locke <stephanie.g.locke@gmail.com>
2017-06-26 07:00:11 +0100

    Verify loop works

--- mkusers.sh
+++ mkusers.sh
@@ -3,5 +3,6 @@ do
         user=`echo $userdetails | cut -f 1 -d ,`
 	passwd=`echo $userdetails | cut -f 2 -d ,`
         useradd $user -m -p `mkpasswd $passwd` -G staff
-        cp sampleSQL.r /home/$user
+        cp sampleSQL.r /home/$user/
+	echo $user
 done
